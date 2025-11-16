516778a1071eea9673586964b935a26a456c81a1
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-16 09:58:16 +0530

    Create index.js

---
+++ app/backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/api/hello", (req, res) => {
+res.json({ message: "Hello from Node backend!" });
+});
+
+
+app.get("/health", (req, res) => res.send("OK"));
+app.get("/ready", (req, res) => res.send("READY"));
+
+
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
