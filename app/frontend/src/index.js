df9a63be7245b474fb7ac9d579b01df5809fe744
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-16 11:21:53 +0530

    Create index.js

---
+++ app/frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+return (
+<div style={{ textAlign: "center", marginTop: "40px", fontFamily: "Arial" }}>
+<h1>React App Running in Docker + Nginx</h1>
+<p>This app was built using the multi-stage Dockerfile you provided.</p>
+</div>
+);
+}
+
+
+export default App;
