16dd81e7110aba1b3a6457dc537c0b04f504cacf
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-16 12:08:03 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -114,18 +114,7 @@ resource"aws_security_group" "sgp" {
     Name = "${var.client_name}-sgp"
   }
 }
-# EC2 -web1
-resource "aws_instance" "web1" {
-  ami           = "ami-0dee22c13ea7a9a67"   
-  instance_type = "t2.micro"
-  key_name      = "new-key-pair"
- 
 
-  tags = {
-    Name = "${var.client_name}-web1"
-   managed_by = "${var.managed_by}"
-  }
-}
 
 
 
